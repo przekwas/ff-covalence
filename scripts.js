7a9b22f697c719abd59b3c185e53f73fd6804151
Luke Przekwas <galactica@The-Galactica.local>
2018-01-16 14:29:47 -0600

    added test arrow

--- scripts.js
+++ scripts.js
@@ -2,12 +2,22 @@
 let infoScreenDiv = $("#info");
 let actionScreenDiv = $("#actions");
 //test playerOne info on the div
-let playerOne = $("<span>Luke 1187 / 3000</span>");
+let playerOne = $("<span>Luke 3000 / 3000</span>");
 playerOne.appendTo(infoScreenDiv);
+
+let arrowPointer = $("<img src='images/si-glyph-arrow-thick-left.svg' id='arrow'>");
+
 
 //loops all the action items on the div
 let actionListItems = ["Fight", "Magic", "Code", "Items"];
 for(i = 0; i < actionListItems.length; i++) {
-    let actionItem = $("<span>" + actionListItems[i] + "</span><br>")
+    let actionItem = $("<span id=" + i + ">" + actionListItems[i] + "</span><br>")
     actionItem.appendTo(actionScreenDiv);
 }
+
+$(document).keypress(function (event) {
+
+arrowPointer.appendTo("#0");
+
+});
+
