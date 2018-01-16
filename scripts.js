8c10f528ed79b937e3def1263d1f99403d42dedb
Luke Przekwas <galactica@Lukes-MacBook-Pro.local>
2018-01-16 10:09:07 -0600

    added comments

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
-let playerOne = $("<span>Luke 1187 / 3000</span>");
+//making calling these divs easier
 let infoScreenDiv = $("#info");
 let actionScreenDiv = $("#actions");
-
+//test playerOne info on the div
+let playerOne = $("<span>Luke 1187 / 3000</span>");
 playerOne.appendTo(infoScreenDiv);
 
+//loops all the action items on the div
 let actionListItems = ["Fight", "Magic", "Code", "Items"];
-
 for(i = 0; i < actionListItems.length; i++) {
     let actionItem = $("<span>" + actionListItems[i] + "</span><br>")
     actionItem.appendTo(actionScreenDiv);
